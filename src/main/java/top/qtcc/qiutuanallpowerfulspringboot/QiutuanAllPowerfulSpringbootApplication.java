e8a90359f4a142d294a071db9d8388e2852cb522
qiutuan <qiutuan0472@163.com>
2024-11-06 20:26:39 +0800

    初始化项目

---
+++ src/main/java/top/qtcc/qiutuanallpowerfulspringboot/QiutuanAllPowerfulSpringbootApplication.java
@@ -0,0 +1,13 @@
+package top.qtcc.qiutuanallpowerfulspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiutuanAllPowerfulSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiutuanAllPowerfulSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/qtcc/qiutuanallpowerfulspringboot/QiutuanAllPowerfulSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package top.qtcc.qiutuanallpowerfulspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiutuanAllPowerfulSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
