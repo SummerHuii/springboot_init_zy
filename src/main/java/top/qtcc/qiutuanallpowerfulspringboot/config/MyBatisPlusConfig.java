462277569a26e8878c20d591808df6f44a067fa0
qiutuan <qiutuan0472@163.com>
2024-12-07 01:57:19 +0800

    MyBatisPlus配置  自动填充功能（自动填充创建时间和更新时间）

--- src/main/java/top/qtcc/qiutuanallpowerfulspringboot/config/MyBatisPlusConfig.java
+++ src/main/java/top/qtcc/qiutuanallpowerfulspringboot/config/MyBatisPlusConfig.java
@@ -1,10 +1,14 @@
 package top.qtcc.qiutuanallpowerfulspringboot.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
 
 @Configuration
 public class MyBatisPlusConfig {
@@ -18,6 +22,28 @@ public class MyBatisPlusConfig {
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
         // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
         return interceptor;
+    }
+
+
+    /**
+     * 自动填充功能（自动填充创建时间和更新时间）
+     *
+     * @return {@link MetaObjectHandler }
+     */
+    @Bean
+    public MetaObjectHandler metaObjectHandler() {
+        return new MetaObjectHandler() {
+            @Override
+            public void insertFill(MetaObject metaObject) {
+                this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+                this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+            }
+
+            @Override
+            public void updateFill(MetaObject metaObject) {
+                this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+            }
+        };
     }
 }
 
