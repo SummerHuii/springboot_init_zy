dda3c00910a183244479a5b85ce12476ec4da145
qiutuan <qiutuan0472@163.com>
2024-12-07 01:49:50 +0800

    缓存配置增强

--- src/main/java/top/qtcc/qiutuanallpowerfulspringboot/config/RedisConfig.java
+++ src/main/java/top/qtcc/qiutuanallpowerfulspringboot/config/RedisConfig.java
@@ -14,37 +14,56 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
+/**
+ *  Redis 配置类
+ *
+ * @author qiutuan
+ * @date 2024/12/07
+ */
 @EnableCaching
 @Configuration
 public class RedisConfig {
 
+    // 配置 RedisTemplate Bean
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-        
-        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+
+        // 使用 StringRedisSerializer 来序列化和反序列化 redis 的 key 值
         template.setKeySerializer(new StringRedisSerializer());
-        // 使用GenericJackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        // 使用 GenericJackson2JsonRedisSerializer 来序列化和反序列化 redis 的 value 值
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        
+
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        
+
         template.afterPropertiesSet();
         return template;
     }
 
+    // 配置 CacheManager Bean
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory factory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofHours(1)) // 缓存过期时间1小时
+                .entryTtl(Duration.ofHours(1)) // 缓存过期时间 1 小时
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-                .disableCachingNullValues();
+                .disableCachingNullValues(); // 禁用缓存空值
 
         return RedisCacheManager.builder(factory)
                 .cacheDefaults(config)
                 .build();
+    }
+
+    // 配置 RedisCacheConfiguration Bean
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1)) // 缓存过期时间 1 小时
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .disableCachingNullValues() // 禁用缓存空值
+                .prefixCacheNameWith("cache:"); // 缓存名称前缀
     }
 }
